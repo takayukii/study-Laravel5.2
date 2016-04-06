5aab563cb2fb13580f8202ec8c2d7280370ae532
takayukii <takayukii@gmail.com>
2016-04-06 17:57:21 +0900

    implements MVC through Article

---
+++ app/Http/Controllers/ArticlesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ArticlesController extends Controller
+{
+    public function index()
+    {
+        $articles = Article::all();
+        return view('articles.index', compact('articles'));
+    }
+
+    public function show($id)
+    {
+        $article = Article::findOrFail($id);
+        return view('articles.show', compact('article'));
+    }
+}
