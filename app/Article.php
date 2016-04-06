0091a3bac32dad8b5be791089569f909250b1d83
takayukii <takayukii@gmail.com>
2016-04-06 17:38:33 +0900

    try mutators

--- app/Article.php
+++ app/Article.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = ['title', 'body', 'published_at'];
+    protected $dates = ['published_at'];
+
+    public function getTitleAttribute($value)
+    {
+        return mb_strtoupper($value);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = mb_strtolower($value);
+    }
 }
