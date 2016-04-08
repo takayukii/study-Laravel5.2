caa2246f09cc8add74c74236ca64bfe99cf92aeb
takayukii <takayukii@gmail.com>
2016-04-08 12:56:54 +0900

    refactor with Route::resource

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,10 +14,6 @@
 Route::get('/', ['as' => 'index', 'uses' => 'WelcomeController@index']);
 Route::get('contact', ['as' => 'contact', 'uses' => 'PagesController@contact']);
 Route::get('about', ['as' => 'about', 'uses' => 'PagesController@about']);
-Route::get('articles', ['as' => 'articles.index', 'uses' => 'ArticlesController@index']);
-Route::get('articles/create', ['as' => 'articles.create', 'uses' => 'ArticlesController@create']);
-Route::get('articles/{id}', ['as' => 'articles.show', 'uses' => 'ArticlesController@show']);
-Route::post('articles', ['as' => 'articles.store', 'uses' => 'ArticlesController@store']);
-Route::get('articles/{id}/edit', ['as' => 'articles.edit', 'uses' => 'ArticlesController@edit']);
-Route::patch('articles/{id}', ['as' => 'articles.update', 'uses' => 'ArticlesController@update']);
-Route::delete('articles/{id}', ['as' => 'articles.destroy', 'uses' => 'ArticlesController@destroy']);
+
+Route::resource('articles', 'ArticlesController');
+
